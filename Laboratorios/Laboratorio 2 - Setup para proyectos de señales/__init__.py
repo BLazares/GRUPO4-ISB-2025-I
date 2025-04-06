7159a339f2dfee950afe84dd5906e95eacf03837
MargarethOD <89661177+MargarethOD@users.noreply.github.com>
2025-04-05 21:37:01 -0500

    Add files via upload

--- Laboratorios/Laboratorio 2 - Setup para proyectos de señales/__init__.py
+++ Laboratorios/Laboratorio 2 - Setup para proyectos de señales/__init__.py
@@ -1,154 +1,87 @@
-"""Private counterpart of ``numpy.typing``."""
+"""Fortran to Python Interface Generator.
 
-from __future__ import annotations
+Copyright 1999 -- 2011 Pearu Peterson all rights reserved.
+Copyright 2011 -- present NumPy Developers.
+Permission to use, modify, and distribute this software is given under the terms
+of the NumPy License.
 
-from ._nested_sequence import (
-    _NestedSequence as _NestedSequence,
-)
-from ._nbit_base import (
-    NBitBase as NBitBase,
-    _8Bit as _8Bit,
-    _16Bit as _16Bit,
-    _32Bit as _32Bit,
-    _64Bit as _64Bit,
-    _80Bit as _80Bit,
-    _96Bit as _96Bit,
-    _128Bit as _128Bit,
-    _256Bit as _256Bit,
-)
-from ._nbit import (
-    _NBitByte as _NBitByte,
-    _NBitShort as _NBitShort,
-    _NBitIntC as _NBitIntC,
-    _NBitIntP as _NBitIntP,
-    _NBitInt as _NBitInt,
-    _NBitLong as _NBitLong,
-    _NBitLongLong as _NBitLongLong,
-    _NBitHalf as _NBitHalf,
-    _NBitSingle as _NBitSingle,
-    _NBitDouble as _NBitDouble,
-    _NBitLongDouble as _NBitLongDouble,
-)
-from ._char_codes import (
-    _BoolCodes as _BoolCodes,
-    _UInt8Codes as _UInt8Codes,
-    _UInt16Codes as _UInt16Codes,
-    _UInt32Codes as _UInt32Codes,
-    _UInt64Codes as _UInt64Codes,
-    _Int8Codes as _Int8Codes,
-    _Int16Codes as _Int16Codes,
-    _Int32Codes as _Int32Codes,
-    _Int64Codes as _Int64Codes,
-    _Float16Codes as _Float16Codes,
-    _Float32Codes as _Float32Codes,
-    _Float64Codes as _Float64Codes,
-    _Complex64Codes as _Complex64Codes,
-    _Complex128Codes as _Complex128Codes,
-    _ByteCodes as _ByteCodes,
-    _ShortCodes as _ShortCodes,
-    _IntCCodes as _IntCCodes,
-    _IntPCodes as _IntPCodes,
-    _IntCodes as _IntCodes,
-    _LongCodes as _LongCodes,
-    _LongLongCodes as _LongLongCodes,
-    _UByteCodes as _UByteCodes,
-    _UShortCodes as _UShortCodes,
-    _UIntCCodes as _UIntCCodes,
-    _UIntPCodes as _UIntPCodes,
-    _UIntCodes as _UIntCodes,
-    _ULongCodes as _ULongCodes,
-    _ULongLongCodes as _ULongLongCodes,
-    _HalfCodes as _HalfCodes,
-    _SingleCodes as _SingleCodes,
-    _DoubleCodes as _DoubleCodes,
-    _LongDoubleCodes as _LongDoubleCodes,
-    _CSingleCodes as _CSingleCodes,
-    _CDoubleCodes as _CDoubleCodes,
-    _CLongDoubleCodes as _CLongDoubleCodes,
-    _DT64Codes as _DT64Codes,
-    _TD64Codes as _TD64Codes,
-    _StrCodes as _StrCodes,
-    _BytesCodes as _BytesCodes,
-    _VoidCodes as _VoidCodes,
-    _ObjectCodes as _ObjectCodes,
-    _StringCodes as _StringCodes,
-    _UnsignedIntegerCodes as _UnsignedIntegerCodes,
-    _SignedIntegerCodes as _SignedIntegerCodes,
-    _IntegerCodes as _IntegerCodes,
-    _FloatingCodes as _FloatingCodes,
-    _ComplexFloatingCodes as _ComplexFloatingCodes,
-    _InexactCodes as _InexactCodes,
-    _NumberCodes as _NumberCodes,
-    _CharacterCodes as _CharacterCodes,
-    _FlexibleCodes as _FlexibleCodes,
-    _GenericCodes as _GenericCodes,
-)
-from ._scalars import (
-    _CharLike_co as _CharLike_co,
-    _BoolLike_co as _BoolLike_co,
-    _UIntLike_co as _UIntLike_co,
-    _IntLike_co as _IntLike_co,
-    _FloatLike_co as _FloatLike_co,
-    _ComplexLike_co as _ComplexLike_co,
-    _TD64Like_co as _TD64Like_co,
-    _NumberLike_co as _NumberLike_co,
-    _ScalarLike_co as _ScalarLike_co,
-    _VoidLike_co as _VoidLike_co,
-)
-from ._shape import (
-    _Shape as _Shape,
-    _ShapeLike as _ShapeLike,
-)
-from ._dtype_like import (
-    DTypeLike as DTypeLike,
-    _DTypeLike as _DTypeLike,
-    _SupportsDType as _SupportsDType,
-    _VoidDTypeLike as _VoidDTypeLike,
-    _DTypeLikeBool as _DTypeLikeBool,
-    _DTypeLikeUInt as _DTypeLikeUInt,
-    _DTypeLikeInt as _DTypeLikeInt,
-    _DTypeLikeFloat as _DTypeLikeFloat,
-    _DTypeLikeComplex as _DTypeLikeComplex,
-    _DTypeLikeTD64 as _DTypeLikeTD64,
-    _DTypeLikeDT64 as _DTypeLikeDT64,
-    _DTypeLikeObject as _DTypeLikeObject,
-    _DTypeLikeVoid as _DTypeLikeVoid,
-    _DTypeLikeStr as _DTypeLikeStr,
-    _DTypeLikeBytes as _DTypeLikeBytes,
-    _DTypeLikeComplex_co as _DTypeLikeComplex_co,
-)
-from ._array_like import (
-    NDArray as NDArray,
-    ArrayLike as ArrayLike,
-    _ArrayLike as _ArrayLike,
-    _ArrayLikeInt as _ArrayLikeInt,
-    _ArrayLikeBool_co as _ArrayLikeBool_co,
-    _ArrayLikeUInt_co as _ArrayLikeUInt_co,
-    _ArrayLikeInt_co as _ArrayLikeInt_co,
-    _ArrayLikeFloat_co as _ArrayLikeFloat_co,
-    _ArrayLikeFloat64_co as _ArrayLikeFloat64_co,
-    _ArrayLikeComplex_co as _ArrayLikeComplex_co,
-    _ArrayLikeComplex128_co as _ArrayLikeComplex128_co,
-    _ArrayLikeNumber_co as _ArrayLikeNumber_co,
-    _ArrayLikeTD64_co as _ArrayLikeTD64_co,
-    _ArrayLikeDT64_co as _ArrayLikeDT64_co,
-    _ArrayLikeObject_co as _ArrayLikeObject_co,
-    _ArrayLikeVoid_co as _ArrayLikeVoid_co,
-    _ArrayLikeStr_co as _ArrayLikeStr_co,
-    _ArrayLikeBytes_co as _ArrayLikeBytes_co,
-    _ArrayLikeString_co as _ArrayLikeString_co,
-    _ArrayLikeAnyString_co as _ArrayLikeAnyString_co,
-    _ArrayLikeUnknown as _ArrayLikeUnknown,
-    _FiniteNestedSequence as _FiniteNestedSequence,
-    _SupportsArray as _SupportsArray,
-    _SupportsArrayFunc as _SupportsArrayFunc,
-    _UnknownType as _UnknownType,
-)
+NO WARRANTY IS EXPRESSED OR IMPLIED.  USE AT YOUR OWN RISK.
+"""
+__all__ = ['run_main', 'get_include']
 
-from ._ufunc import (
-    _UFunc_Nin1_Nout1 as _UFunc_Nin1_Nout1,
-    _UFunc_Nin2_Nout1 as _UFunc_Nin2_Nout1,
-    _UFunc_Nin1_Nout2 as _UFunc_Nin1_Nout2,
-    _UFunc_Nin2_Nout2 as _UFunc_Nin2_Nout2,
-    _GUFunc_Nin2_Nout1 as _GUFunc_Nin2_Nout1,
-)
+import sys
+import subprocess
+import os
+import warnings
+
+from numpy.exceptions import VisibleDeprecationWarning
+from . import f2py2e
+from . import diagnose
+
+run_main = f2py2e.run_main
+main = f2py2e.main
+
+
+def get_include():
+    """
+    Return the directory that contains the ``fortranobject.c`` and ``.h`` files.
+
+    .. note::
+
+        This function is not needed when building an extension with
+        `numpy.distutils` directly from ``.f`` and/or ``.pyf`` files
+        in one go.
+
+    Python extension modules built with f2py-generated code need to use
+    ``fortranobject.c`` as a source file, and include the ``fortranobject.h``
+    header. This function can be used to obtain the directory containing
+    both of these files.
+
+    Returns
+    -------
+    include_path : str
+        Absolute path to the directory containing ``fortranobject.c`` and
+        ``fortranobject.h``.
+
+    Notes
+    -----
+    .. versionadded:: 1.21.1
+
+    Unless the build system you are using has specific support for f2py,
+    building a Python extension using a ``.pyf`` signature file is a two-step
+    process. For a module ``mymod``:
+
+    * Step 1: run ``python -m numpy.f2py mymod.pyf --quiet``. This
+      generates ``mymodmodule.c`` and (if needed)
+      ``mymod-f2pywrappers.f`` files next to ``mymod.pyf``.
+    * Step 2: build your Python extension module. This requires the
+      following source files:
+
+      * ``mymodmodule.c``
+      * ``mymod-f2pywrappers.f`` (if it was generated in Step 1)
+      * ``fortranobject.c``
+
+    See Also
+    --------
+    numpy.get_include : function that returns the numpy include directory
+
+    """
+    return os.path.join(os.path.dirname(__file__), 'src')
+
+
+def __getattr__(attr):
+
+    # Avoid importing things that aren't needed for building
+    # which might import the main numpy module
+    if attr == "test":
+        from numpy._pytesttester import PytestTester
+        test = PytestTester(__name__)
+        return test
+
+    else:
+        raise AttributeError("module {!r} has no attribute "
+                              "{!r}".format(__name__, attr))
+
+
+def __dir__():
+    return list(globals().keys() | {"test"})
