2b41d61202e6d55256990c6410585c9602f04e6b
MargarethOD <89661177+MargarethOD@users.noreply.github.com>
2025-04-05 21:59:08 -0500

    Add files via upload

--- Laboratorios/Laboratorio 2 - Setup para proyectos de señales/arrays/__init__.py
+++ Laboratorios/Laboratorio 2 - Setup para proyectos de señales/arrays/__init__.py
@@ -1,27 +1,33 @@
-"""
-All of pandas' ExtensionArrays.
-
-See :ref:`extending.extension-types` for more.
-"""
-from pandas.core.arrays import (
-    ArrowExtensionArray,
-    ArrowStringArray,
-    BooleanArray,
-    Categorical,
-    DatetimeArray,
-    FloatingArray,
-    IntegerArray,
-    IntervalArray,
-    NumpyExtensionArray,
-    PeriodArray,
-    SparseArray,
-    StringArray,
-    TimedeltaArray,
+from pandas.core.arrays.arrow import ArrowExtensionArray
+from pandas.core.arrays.base import (
+    ExtensionArray,
+    ExtensionOpsMixin,
+    ExtensionScalarOpsMixin,
 )
+from pandas.core.arrays.boolean import BooleanArray
+from pandas.core.arrays.categorical import Categorical
+from pandas.core.arrays.datetimes import DatetimeArray
+from pandas.core.arrays.floating import FloatingArray
+from pandas.core.arrays.integer import IntegerArray
+from pandas.core.arrays.interval import IntervalArray
+from pandas.core.arrays.masked import BaseMaskedArray
+from pandas.core.arrays.numpy_ import NumpyExtensionArray
+from pandas.core.arrays.period import (
+    PeriodArray,
+    period_array,
+)
+from pandas.core.arrays.sparse import SparseArray
+from pandas.core.arrays.string_ import StringArray
+from pandas.core.arrays.string_arrow import ArrowStringArray
+from pandas.core.arrays.timedeltas import TimedeltaArray
 
 __all__ = [
     "ArrowExtensionArray",
+    "ExtensionArray",
+    "ExtensionOpsMixin",
+    "ExtensionScalarOpsMixin",
     "ArrowStringArray",
+    "BaseMaskedArray",
     "BooleanArray",
     "Categorical",
     "DatetimeArray",
@@ -30,24 +36,8 @@ __all__ = [
     "IntervalArray",
     "NumpyExtensionArray",
     "PeriodArray",
+    "period_array",
     "SparseArray",
     "StringArray",
     "TimedeltaArray",
 ]
-
-
-def __getattr__(name: str) -> type[NumpyExtensionArray]:
-    if name == "PandasArray":
-        # GH#53694
-        import warnings
-
-        from pandas.util._exceptions import find_stack_level
-
-        warnings.warn(
-            "PandasArray has been renamed NumpyExtensionArray. Use that "
-            "instead. This alias will be removed in a future version.",
-            FutureWarning,
-            stacklevel=find_stack_level(),
-        )
-        return NumpyExtensionArray
-    raise AttributeError(f"module 'pandas.arrays' has no attribute '{name}'")
